83ca896e8a17dbb5a09fae047167381d17397e78
thayaagh <thayaagh@students.zhaw.ch>
2024-05-20 22:38:46 +0200

    Presentation

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps DEMO PRESENTATION!');
+	res.send('DEMO PRESENTATION!');
 });
 
 app.listen(PORT, HOST);
