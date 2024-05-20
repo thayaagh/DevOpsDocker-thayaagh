2b2e8dac7ad7da0a04e72e18d2603289d43b8595
thayaagh <thayaagh@students.zhaw.ch>
2024-05-20 22:56:44 +0200

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('CHANGE FOR DEMO PRESENTATION!');
+	res.send('CHANGE FOR DEMO PRESENTATION! :-)');
 });
 
 app.listen(PORT, HOST);
